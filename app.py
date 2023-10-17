37ea8c8cffe4f55d11f45b55b92c7ff3c5b9d9b0
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2023-10-17 14:42:56 -0300

    Additionally, CORS and Prometheus Metrics have been imported and configured, and a ".gitignore" file has been added to exclude certain files and directories from Git tracking. This will help in managing version control and enhance application monitoring and performance tracking.

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
 
+from flask_cors import CORS
+from prometheus_flask_exporter import PrometheusMetrics
+
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+metrics = PrometheusMetrics(app, group_by='endpoint', default_labels={'application': 'CscTrackerPoint'})
 
 
 @app.route('/')
@@ -9,4 +16,4 @@ def hello_world():  # put application's code here
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
